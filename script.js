20a83dfed44d12b0d990e540bc85a41495fc16a0
John Mark Alvis <100738110+alvisjohnmark@users.noreply.github.com>
2023-05-15 20:47:45 +0800

    upload
    upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// HEADER: SEARCH
+
+const triggerOpen = document.querySelectorAll('[trigger-button]');
+const triggerClose = document.querySelectorAll('[close-button]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < triggerOpen.length; i++) {
+	let currentId = triggerOpen[i].dataset.target,
+	targetEl = document.querySelector(`#${currentId}`)
+
+	const openData = function() {
+		targetEl.classList.remove('active');
+		overlay.classList.remove('active');
+	};
+	triggerOpen[i].addEventListener('click', function() {
+		targetEl.classList.add('active');
+		overlay.classList.add('active');
+	});
+
+	targetEl.querySelector('[close-button]').addEventListener('click', openData);
+	overlay.addEventListener('click', openData);
+}
+
+// Mobile- menu sub-menu
+const submenu = document.querySelectorAll('.child-trigger');
+submenu.forEach((menu) => menu.addEventListener('click', function(e) {
+	e.preventDefault();
+	submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
+	if(this.closest('.has-child').classList != 'active') {
+		this.closest('.has-child').classList.toggle('active');
+	}
+}))
+
+// Slider 
+const swiper = new Swiper('.sliderbox', {
+
+  loop: true,
+  effect: 'fade',
+  autoHeight: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+});
+
